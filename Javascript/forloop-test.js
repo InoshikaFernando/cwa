c2219a0b517ff57c03e8ccc75e8a82377908b06f
~inoshika <inoshi.fernando@gmail.com>
2025-09-23 21:12:23 +1200

    Adding more cords

---
+++ Javascript/forloop-test.js
@@ -0,0 +1,31 @@
+/**
+👉 Create an array of your 5 favorite animals.
+👉 Use a for loop to print them.
+👉 Use a for loop to print them reverse order.
+👉 Use a forEach loop to print them.
+👉 Which do you like better?
+ */
+
+const animals = ['Dog', 'Cat', 'Elephant', 'Giraffe', 'Lion'];
+
+function forloop() {
+    for (let i = 0; i < animals.length; i++) {
+	console.log(animals[i]);
+    } 
+}
+
+
+function forloopReverse() {
+    for (let i = animals.length - 1; i >= 0; i--) {
+	console.log(animals[i]);
+    }
+}
+
+function foreach() {
+    for (const object of objects) {
+        
+    }
+}
+
+forloop();
+forloopReverse();
---
+++ Javascript/testCalculator.js
@@ -0,0 +1,21 @@
+/**
+ * Create a function call 'addition' which takes 2 parameters, and return the addition of it
+ * So when you call 'addition(2,3)', it should return 5
+ */
+function addition(x,y) {
+    return x+y;
+}
+
+/**
+ * Create a function call substraction, which takes 2 parameters, and returns the substraction value
+ * So when you call substraction(5, 4), it should return 1
+ */
+
+
+
+function test() {
+    console.log(addition(2,3))
+    console.log()
+}
+
+test()
